e21f1fd456ec8bdfaecaf0fdc8fe1a2352562fb3
Егор Рачев <rachjoves@std.tyuiu.ru>
2023-09-11 17:29:09 +0500

    Task2

---
+++ Tyuiu.RACHEVes.Sprint0.Task2.V0/Tyuiu.RACHEVes.Sprint0.Task2.V0.cpp
@@ -0,0 +1,10 @@
+// Tyuiu.RACHEVes.Sprint0.Task2.V0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
