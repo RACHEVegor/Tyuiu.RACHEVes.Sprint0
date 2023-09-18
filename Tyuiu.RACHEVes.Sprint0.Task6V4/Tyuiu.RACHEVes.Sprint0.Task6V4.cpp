2aad0fd1287b4084326da7e4a6e3735dd54fafd4
Егор Рачев <rachjoves@std.tyuiu.ru>
2023-09-18 16:37:27 +0500

    Task6

---
+++ Tyuiu.RACHEVes.Sprint0.Task6V4/Tyuiu.RACHEVes.Sprint0.Task6V4.cpp
@@ -0,0 +1,18 @@
+// Tyuiu.RACHEVes.Sprint0.Task6V4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, y;
+    cout << "¬введите число x -";
+	cin >> x;
+	cout << "¬ведите число y -";
+	cin >> y;
+	double D = (5 * x) / (7 + y);
+	cout << "–ешение примера (5 * x) / (7 + y) = "<< D x<< endl;
+	return 0;
+}
